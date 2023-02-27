2effd4a30523c04e5139e3b510ddf4e62611f76a
Denis Cifuentes <denisjoelcif@gmail.com>
2023-02-26 18:17:52 -0600

    prueba04

--- app/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ app/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -16,7 +16,7 @@ public class ClienteController {
 
     @RequestMapping(value="listar", method = RequestMethod.GET)
     public String listar(Model model){
-        model.addAttribute("titulo", "Listado de clientes :3");
+        model.addAttribute("titulo", "Listado de clientes");
         model.addAttribute("clientes", clienteDao.findAll());
         return "listar";
     }
